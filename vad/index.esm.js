a0abf35fe8df02b52057ee4877f1895b9b0b4827
darajwani <darajwani@gmail.com>
2025-05-22 10:12:12 +0100

    index.esm.js

--- vad/index.esm.js
+++ vad/index.esm.js
@@ -1,1 +1,20 @@
-// VAD module entry (placeholder for index.esm.js)
+export default class VAD {
+  static async new(options) {
+    console.log("VAD.new() called with stream:", options.stream);
+    return {
+      waitForSpeechStart: async () => {
+        console.log("Mock VAD: waiting for speech start...");
+        await new Promise((res) => setTimeout(res, 1000));
+        return true;
+      },
+      waitForSpeechEnd: async () => {
+        console.log("Mock VAD: waiting for speech end...");
+        await new Promise((res) => setTimeout(res, 1000));
+        return true;
+      },
+      destroy: () => {
+        console.log("Mock VAD: destroyed");
+      }
+    };
+  }
+}
